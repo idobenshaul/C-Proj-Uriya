0f1cadbff889fa94f47f52e7425b8c042830908d
jreing <jreing@users.noreply.github.com>
2015-05-12 19:28:47 +0300

    Update logic.h

--- logic.h
+++ logic.h
@@ -14,8 +14,16 @@
 #define RIGHT 2
 #define DOWN 3
 #define LEFT 4
-int mouseRow, mouseCol, catRow, catCol, cheeseX, cheeseY;
 
+#define GOOD_WORLD 0
+#define CAT_MISSING 1
+#define MOUSE_MISSING 2
+#define CHEESE_MISSING 3
+
+
+int mouseRow, mouseCol, catRow, catCol, cheeseRow, cheeseCol;
+
+int consoleMode ();
 void board_init(char*** board);
 int evalFunc(void* state);
 void switchTurn();
@@ -26,7 +34,7 @@ int updateGameStatus(char** board);
 void printBoard(char** board);
 void copyArray(char *** original, char***copy);
 char turn[7];//toggle cat's turn.
-int isOriginalTurn=1;
+int isOriginalTurn;
 int max_turns ; //max num of turns.
 int turnCounter;
 int evalFunc(void* state);
@@ -35,9 +43,16 @@ int checkBoard(char** board);
 int saveGame(char** board, int gamenum);
 void makeOriginalBoard(char** board);
 void switchIsOriginalTurn();
-int cntFailedMoves=0;
-int badIndex;
-
+int isLegalMove(char*** board, int srcRow, int srcCol, int direction);
+int findNeighborBFS(char***board, int mouseR,int mouseC,int cheeseR,int cheeseC);
+//int cntFailedMoves=0;
+//int badIndex;
+int visited[7][7];
+int dist[7][7];
+struct MiniMaxResult wrapGetBestChild(int actualCatRow, int actualCatCol,int actualMouseRow,int  actualMouseCol,
+		void* state, unsigned int maxDepth,
+		        ListRef (*getChildren) (void* state),FreeFunc freeState,
+		        int (*evaluate) (void* state), int isMaxPlayer);
 struct gameOptions {
 	int cat_human;
 	int mouse_human;
